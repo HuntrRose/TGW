fe8c4ae645a2142f530dbc5e792175dc65872579
grendel <simen.rosness@gmail.com>
2016-06-17 16:46:48 +0200

    Multivalue for Unit Images

---
+++ TGWv2/ViewModel/UnitMultiConverter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+using UnitLib;
+
+namespace TGWv2.ViewModel
+{
+    class UnitMultiConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            
+            if ((bool)values[1])
+            {
+                if (values[0] is InfantryUnit)
+                {
+                    throw new NotImplementedException();
+                }
+                else if (values[0] is ArmoredCarUnit)
+                {
+                    /*string image = ".\\img\\unitACGreen.png";
+                    return image;*/
+                    return new BitmapImage(new Uri(".\\img\\unitACGreen.png", UriKind.RelativeOrAbsolute));
+                }
+                //else if (value is ArtilleryUnit)
+                else
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
